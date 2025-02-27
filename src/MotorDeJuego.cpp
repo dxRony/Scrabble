486b1346d4f7c32b8d55e0a9a1ec9f8bfb769740
dxRony <romaroag@gmail.com>
2025-02-27 13:43:29 -0600

    agregando manejo de errores en menu principal

--- src/MotorDeJuego.cpp
+++ src/MotorDeJuego.cpp
@@ -2,12 +2,11 @@
 // Created by ronyrojas on 26/02/25.
 //
 #include "../include/MotorDeJuego.h"
-
-
 #include <iostream>
-
+#include <limits>
 #include "../include/Archivo.h"
 #include "../include/Partida.h"
+
 using namespace std;
 
 MotorDeJuego::MotorDeJuego() {
@@ -15,29 +14,42 @@ MotorDeJuego::MotorDeJuego() {
 
 void MotorDeJuego::mostrarMenu() {
     int opcion;
-
     do {
         cout << "            BIENVENIDO A SCRABBLE   " << endl;
         cout << "\n*********** Menu Principal ***********" << endl;
         cout << "1. Nueva Partida" << endl;
         cout << "2. Ver Reportes" << endl;
         cout << "3. Salir Del Juego" << endl;
-        cout << "Selecciona Una Opcion" << endl;
-        cin >> opcion;
+        cout << "Selecciona Una Opcion: ";
 
-        switch (opcion) {
-            case 1:
-                nuevaPartida();
-            break;
-            case 2:
-                verReportes();
-            break;
-            case 3:
-                finalizarEjecucion = true;
-            break;
-            default:
-                cout << "Opcion ingresada no válida." << endl;
-            break;
+        try {
+            cin >> opcion;
+
+            // Verificar si la entrada es válida
+            if (cin.fail()) {
+                throw runtime_error("Entrada invalida, debes ingresar un numero entre 1 y 3");
+            }
+
+            if (opcion < 1 || opcion > 3) {
+                throw out_of_range("Opcion fuera de rango, debes ingresar un numero entre 1 y 3");
+            }
+
+            // Si la entrada es válida, ejecutar la opción
+            switch (opcion) {
+                case 1:
+                    nuevaPartida();
+                    break;
+                case 2:
+                    verReportes();
+                    break;
+                case 3:
+                    finalizarEjecucion = true;
+                    break;
+            }
+        } catch (const exception &e) {
+            cout << "Error: " << e.what() << endl;
+            cin.clear(); // limpiando entrada en consola
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignorando entrada incorrecta
         }
     } while (!finalizarEjecucion);
 }
@@ -46,7 +58,7 @@ void MotorDeJuego::nuevaPartida() {
     cout << "\nIniciando Nueva Partida..." << endl;
     Archivo archivo;
     cout << "\nLeyendo y ordenando palabras..." << endl;
-    ListaEnlazada<Palabra> *listaPalabras= archivo.ordenarAlfabeticamente();
+    ListaEnlazada<Palabra> *listaPalabras = archivo.ordenarAlfabeticamente();
     cout << "Lista de palabras disponibles para la partida:" << endl;
     listaPalabras->imprimirLista();
 
